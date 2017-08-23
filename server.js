8b70cbe4e4d64c4b14039c6f4a149a571c822e85
Jananiram3 <30656532+Jananiram3@users.noreply.github.com>
2017-08-23 22:31:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function(req,res){
+    res.send('article one')
+});
+app.get('/article2', function(req,res){
+    res.send('article two')
+});
+app.get('/article3', function(req,res){
+    res.send('article three')
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
